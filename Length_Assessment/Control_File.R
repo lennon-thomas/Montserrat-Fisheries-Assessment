e5c17c4015403d222190d8bb07e26166a56708bf
alxsmith <aedwards.smith@gmail.com>
2016-10-28 14:12:30 -0700

    Control file is working for single app. Still need to loop to run multi spp

--- Length_Assessment/Control_File.R
+++ Length_Assessment/Control_File.R
@@ -6,7 +6,11 @@
 
 rm(list = ls())
 
-pkgs <- c('plyr','readr', 'ggplot2','tidyr', 'R2admb', 'dplyr')
+if (basename(getwd())!='Montserrat-Fisheries-Assessment'){
+  print("Set working directory to home folder")
+}
+
+pkgs <- c('plyr','readr', 'ggplot2','tidyr', 'R2admb', 'dplyr', 'animation')
 
 #This will install necessary packages on local machine
 load.packages = function(a){
@@ -20,15 +24,41 @@ lapply(pkgs, load.packages)
 
 #Source functions to file
 sapply(list.files(pattern="[.]R$", path="./Length_Assessment/Functions", full.names=TRUE), source)
+source("./Length_Assessment/Functions/SubFunctions.R") #Pull in helper functions for assessment modules
 
+############High Level Assessment Controls############
 
 #Assessments to run with control file
 Assessments <- c('LBAR')
 
 Counter<- 0
 
-#Load and summarize length data
-LengthData <- read.csv("./Length_Assessment/all_length.csv") %>%
+Sites<- c('All')
+
+AssessmentResults<- list()
+
+MonteResults<- list()
+
+Assessment <- 'Doctorfish'
+
+dir.create(Assessment)
+
+NumIterations <- 100
+
+SPRRef <- 0.4
+
+CPUERef <- 0.4
+
+RunAssessments <- TRUE 
+
+NumberOfSpecies <- 1
+
+ReserveYear <- NA
+
+MinSampleSize <- 180
+
+############################Load and summarize length data######################
+LengthData <- read.csv("./Length_Assessment/all_length_dates.csv") %>%
   filter(Species.ID == "ACANCH")
 
 names(LengthData)
@@ -47,31 +77,147 @@ common<-c(
   LUTJMA="Mahogany snapper",
   LUTJSY="Lane snapper",
   LUTJVI="Silk snapper",
-  SCARCH="Spotlight parrotfish", # this should be Redtail Parrotfish?
+  SCARCH="Redtail parrotfish", # this should be Redtail Parrotfish?
   SERRGU="Red hind")
 
-######################################################################################
-#LBAR
+
+#######################Life History#############################################
+
+# Fish<-read.csv("./Length_Assessment/MNI_LH_FINAL.csv")%>%
+#   filter(Species==sp_id[1]) %>%
+#   select(-Reference) %>%
+#   spread(Parameter, Value)
+# 
+# Fish$AgeMat50<- NA
+# 
+# Fish$AgeMatSource<- NA
+# 
+# Fish$MaxAge <- ceiling(-log(0.05)/Fish$M)
+
+#######################Species LH Control File#############################################
+source(paste('./Length_Assessment/Species_Control_Files/Montserrat2-LittleBay-Doctorfish_ControlFile.R'))
+
+##Lennon commented these out
+# LifeHistory<-read.csv(paste(Assessment,'/',Files[grepl('_LifeHistory',Files)],sep=''), stringsAsFactors = F)
+#
+
+LifeHistory<-melt(Fish,na.rm=T)
+LifeHistory<-as.matrix(LifeHistory)
+LifeHistory<-t(LifeHistory)
+colnames(LifeHistory)<-LifeHistory[2,]
+LifeHistory<-LifeHistory[1,]
+LifeHistory<-t(LifeHistory)
+LifeHistory<-LifeHistory[-2,]
+LifeHistory<-as.data.frame(LifeHistory,stringsAsFactors =FALSE)
+
+
+LifeHistory<-data.frame(t(LifeHistory),stringsAsFactors = FALSE)
+
+Fishes <- unique(Fish$CommName)
+
+
+LifeData<- colnames(LifeHistory)[5:dim(LifeHistory)[2]]
+
+Fish$AgeMat50<- NA
+
+Fish$AgeMatSource<- NA
+
+
+Sites<- c('All')
+
+AssessmentResults<- list()
+
+MonteResults<- list()
+
+
+
+# Run Assessments ---------------------------------------------------------
+s=1
+
+# if (RunAssessments==T)
+# {
+#   
+#   for (s in 1:length(Sites))    
+#   {
+
+show(Sites[s])
+
+f=1
+
+
+show(Fishes[f])
+
+Species<- Fishes[f]
+
+
+AssessmentName <- paste(Assessment,Sites[s],Fishes[f],sep='_')
+
+Directory<- paste(Assessment, "/", Sites[s],'/',Fishes[f],'/',sep='')
+
+if (file.exists(Directory)==F)
+{
+  dir.create(paste(Assessment, "/", Sites[s],sep=''))
+  dir.create( paste(Assessment, "/", Sites[s],'/',Fishes[f],'/',sep=''))
+}
+
+# Prepare Fish Object -------------------------------------------------------------
+s=1
+SpeciesLifeHistory<- LifeHistory[LifeHistory$CommName == Species[s],colnames(LifeHistory) %in% LifeData]
+SpeciesLifeHistory<-LifeHistory
+
+SpeciesLifeHistory[,3:18] <- as.numeric(SpeciesLifeHistory[,3:18])
+
+sTaxa<-"Surgeonfish"
+HasLifeHistory<- SpeciesLifeHistory[which(is.na(SpeciesLifeHistory)==F)]
+
+HasLife<- colnames(HasLifeHistory)
+
+Fish$CommName <- Species[s]
+
+
+
+##########CREATES FOLDERS##########     
+FigureFolder<- paste(Directory,'Figures/',sep='')
+
+ResultFolder<- paste(Directory,'Results/',sep='')
+
+if (file.exists(FigureFolder)==F)
+{
+  dir.create(FigureFolder,recursive=T)
+  
+  dir.create(ResultFolder,recursive=T)
+}
+
+
+
+a=1
+Fish$LHITol<- 0.99
+#       
+#       for (a in 1:length(Assessments)) #Loop over possible assessments, store in Assessment results. Many assessments have more detailed outputs than can also be accessed 
+#       {
 
 Counter<- Counter+1
+#         if (Assessments[a]=='LBAR') #Run LBAR assessment
+#         {
+#           
+SampleCheck<- CheckLengthSampleSize(LengthData)        
+#           
+#           if (SampleCheck$YearsWithEnoughData>0)
+#           {
 
-SampleCheck<- CheckLengthSampleSize(LengthData)   
 
-Temp<- LBAR(SampleCheck$ParedData,LagLength=1,Weight=1,IncludeMPA=0,ReserveYr=NA,OutsideBoundYr=NA,Iterations=1,
-            BootStrap=0,LifeError=0,Lc=NA)$Output		
+Temp<- LBAR(SampleCheck$ParedData,LagLength=1,Weight=1,IncludeMPA=0,ReserveYr=NA,OutsideBoundYr=NA,Iterations=1000,
+            BootStrap=1,LifeError=0,Lc=18)$Output		
 
 StoreAssess<- data.frame(Species,Sites[s],Assessments[a],Temp,stringsAsFactors=F) %>%
   rename(Site=Sites.s.,Assessment=Assessments.a.)
 
 AssessmentResults[[Counter]]<-StoreAssess
+#######################Save Results#############################################
 
+CurrentResults<- ldply(AssessmentResults) %>% subset(Species==Fishes[f] & Site==Sites[s])
 
-######################################################################################
-#LBSPR
-
-
-
-
+write.csv(file=paste(ResultFolder,AssessmentName,'_Results.csv',sep=''),CurrentResults)
 #######################Plots#############################################
 
 #Font <- 'Helvetica'
@@ -90,8 +236,11 @@ for (i in 1:length(sp_id)){
     spread(Parameter, Value)
   
 
-  Fish$Mat50<-Fish$m95
+  Fish$Mat50 <- Fish$m95
 
+ 
+  
+  
 #Specifies where to save plots
   FigureFolder<- paste("./Length_Assessment/plots/")
   name="Data Type"
